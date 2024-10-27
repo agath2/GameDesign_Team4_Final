992807d3706399a2729a41f3cd79f7a7d4183592
miat603 <miat603@gmail.com>
2024-10-27 16:04:59 -0400

    Tilemap, button that opens door
    also added camera bounding script

---
+++ Team4_FinalProj/Assets/Scripts/WallToggle.cs
@@ -0,0 +1,54 @@
+using System.Collections.Generic;
+using System.Collections;
+using UnityEngine;
+
+public class WallToggle : MonoBehaviour
+{
+    public int channel = 0;
+    public bool startOn = true;
+    private bool toggledOn = true;
+    public GameObject wallUp;
+    public GameObject wallDown;
+
+    void Start()
+    {
+        wallUp.SetActive(true);
+        wallDown.SetActive(false);
+
+        GameObject[] buttonArr = GameObject.FindGameObjectsWithTag("WallToggleButton");
+        foreach (GameObject button in buttonArr)
+        {
+            if (button.GetComponent<WallToggleButton>().channel == channel)
+            {
+                button.GetComponent<WallToggleButton>().wallList.Add(this);
+            }
+        }
+
+        if (!startOn) { ToggleState(); }
+
+   
+    }
+
+    public void ToggleState()
+    {
+        toggledOn = !toggledOn;
+        if (toggledOn)
+        {
+            wallUp.SetActive(true);
+            wallDown.SetActive(false);
+            GetComponent<BoxCollider2D>().enabled = true;
+        }
+        else
+        {
+            wallUp.SetActive(false);
+            wallDown.SetActive(true);
+            GetComponent<BoxCollider2D>().enabled = false;
+            Debug.Log("Wall gone");
+        }
+    }
+
+    public void SetColor(Color col)
+    {
+        GetComponentInChildren<SpriteRenderer>().color = col;
+    }
+}
