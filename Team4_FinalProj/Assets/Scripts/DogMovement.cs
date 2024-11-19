a053320da9b2d8c3d9dbaf8a952d2f5e26976427
miat603 <miat603@gmail.com>
2024-11-19 00:11:17 -0500

    re-added fetch script and key to mia scene

--- Team4_FinalProj/Assets/Scripts/DogMovement.cs
+++ Team4_FinalProj/Assets/Scripts/DogMovement.cs
@@ -31,6 +31,7 @@ public class DogMovement : MonoBehaviour
         mapManager = FindObjectOfType<MapManager>();
     }
 
+
     void Update()
     {
 
@@ -49,6 +50,18 @@ public class DogMovement : MonoBehaviour
         else if (followPlayer)
         {
             HandlePlayerFollowing();
+        }
+    }
+
+    public bool isStopped()
+    {
+        if (!moveToTarget && !followPlayer)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
         }
     }
 
