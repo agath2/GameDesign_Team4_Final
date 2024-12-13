975fc7eef3c183ca3ada573c4b1ad6db0416bf52
Jack-86 <jack.l008600@gmail.com>
2024-12-13 14:39:41 -0500

    Added coin pickup sound

--- Team4_FinalProj/Assets/Scripts/CoinPickUp.cs
+++ Team4_FinalProj/Assets/Scripts/CoinPickUp.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class CoinPickup : MonoBehaviour
 {
     public int coinValue = 1;  // The value of each coin, default to 1
+    public AudioClip pickupSound;  // The sound to play upon pickup
     
     // This method is called when a collision with a trigger occurs
     private void OnTriggerEnter2D(Collider2D other)
@@ -12,6 +13,9 @@ public class CoinPickup : MonoBehaviour
             // Add to the player's coin count
             CoinManager.instance.AddCoins(coinValue);
             
+            // Play the pickup sound at the coin's position
+            AudioSource.PlayClipAtPoint(pickupSound, transform.position);
+
             // Destroy the coin (make it disappear)
             Destroy(gameObject);
         }
