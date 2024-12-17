01f2b4de50c17c2177f6777e20a6d723af4c850c
kmuhle <kalyn.muhlenberg@tufts.edu>
2024-12-16 23:41:09 -0500

    Fixed coins counting twice

--- Team4_FinalProj/Assets/Scripts/CoinPickUp.cs
+++ Team4_FinalProj/Assets/Scripts/CoinPickUp.cs
@@ -4,14 +4,18 @@ public class CoinPickup : MonoBehaviour
 {
     public int coinValue = 1;  // The value of each coin, default to 1  
     // public AudioSource pickupSound;  // The sound to play upon pickup
+    public bool isPickedUp = false;
     
     // This method is called when a collision with a trigger occurs
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player") && !isPickedUp)
         {
+            isPickedUp = true;
+            Debug.Log("Coin picked up");
             // Add to the player's coin count
             CoinManager.instance.AddCoins(coinValue);
+            GetComponent<Collider2D>().enabled = false;
             
             // Play the pickup sound at the coin's position
                 // pickupSound.Play();
